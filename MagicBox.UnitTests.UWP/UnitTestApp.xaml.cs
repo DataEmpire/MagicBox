87aa77ca37f47361ac739e2bb026bc2bb78692d3
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2015-12-04 01:53:13 -0200

    The initial commit for the open-source project: MagicBox. This first commit already supports: A portable and uwp project. And xunit test project for the uwp project.

---
+++ MagicBox.UnitTests.UWP/UnitTestApp.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+
+namespace MagicBox.UnitTests.UWP
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the RunnerApplication class.
+    /// </summary>
+    public sealed partial class App
+    {
+        /// <summary>
+        /// Set that the tests can be inside the main assembly.
+        /// </summary>
+        protected override void OnInitializeRunner()
+        {
+            AddTestAssembly(GetType().GetTypeInfo().Assembly);
+        }
+    }
+}
