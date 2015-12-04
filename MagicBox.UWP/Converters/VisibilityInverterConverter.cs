17248ca4808cc54231f682178a30156c3d73862e
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2015-12-04 03:25:25 -0200

    Added a new tested converter: VisibilityInverterConverter. It is used for example to hide a control when other is visible and vice versa.

---
+++ MagicBox.UWP/Converters/VisibilityInverterConverter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace MagicBox.UWP.Converters
+{
+    /// <summary>
+    /// Inverter by two-way binding a <see cref="Visibility"/> value.
+    /// </summary>
+    /// <remarks>
+    /// in:                             out:
+    /// Visibility.Visible              Visibility.Collapsed
+    /// Visibility.Collapsed            Visibility.Visible
+    /// </remarks>
+    public class VisibilityInverterConverter : IValueConverter
+    {
+        /// <summary>
+        /// Converter a valid Visibility value in its inverted value.
+        /// </summary>
+        /// <param name="value">A valid <see cref="Visibility"/> value.</param>
+        /// <param name="targetType">The type for <see cref="Visibility"/>.</param>
+        /// <param name="parameter">This parameter can be used to change the default logic flow.</param>
+        /// <param name="language">The current language on the app.</param>
+        /// <returns>The inversed value for the Visibility entered.</returns>
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (!(value is Visibility))
+            {
+                throw new ArgumentException("the value must be a Windows.UI.Xaml.Visibility object");
+            }
+
+            var visibility = (Visibility)value;
+
+            return (visibility == Visibility.Collapsed) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        /// <summary>
+        /// Already not implemented once that this method is called when target updates source object.
+        /// </summary>
+        /// <param name="value">A <see cref="Visibility"/> value.</param>
+        /// <param name="targetType">The type indicator to the <see cref="Visibility"/> type.</param>
+        /// <param name="parameter">This parameter can be used to change the default logic flow.</param>
+        /// <param name="language">The current language on the app.</param>
+        /// <returns>A new object of a <see cref="NotImplementedException"/>.</returns>
+        /// <exception cref="NotImplementedException">I don't see a situation in that this can be used or invoked.</exception>
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MagicBox.UnitTests.UWP/Converters/VisibilityInverterConverterFixture.cs
@@ -0,0 +1,62 @@
+﻿using MagicBox.UWP.Converters;
+using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+using Xunit;
+
+namespace MagicBox.UnitTests.UWP.Converters
+{
+    /// <summary>
+    /// The validation tests for the <see cref="VisibilityInverterConverter"/>.
+    /// </summary>
+    public class VisibilityInverterConverterFixture : IDisposable
+    {
+        private IValueConverter _converter;
+
+        public VisibilityInverterConverterFixture()
+        {
+            _converter = new VisibilityInverterConverter();
+        }
+
+        /// <summary>
+        /// Clear all fields.
+        /// </summary>
+        public void Dispose()
+        {
+            _converter = null;
+        }
+
+        /// <summary>
+        /// Verify if the inverter of an Visibility value it is working.
+        /// </summary>
+        [Fact]
+        public void VerifyConverter()
+        {
+            var expected = Visibility.Collapsed;
+            var actual = _converter.Convert(Visibility.Visible, typeof(Visibility), null, string.Empty);
+
+            Assert.Equal(expected, actual);
+
+            expected = Visibility.Visible;
+            actual = _converter.Convert(Visibility.Collapsed, typeof(Visibility), null, string.Empty);
+
+            Assert.Equal(expected, actual);
+
+            Action testCode = () => _converter.Convert(null, typeof (Visibility), null, string.Empty);
+
+            Assert.Throws<ArgumentException>(testCode);
+        }
+
+        /// <summary>
+        /// Verify the behaviors on the ConverterBack method call.
+        /// </summary>
+        [Fact]
+        public void VerifyConverterBack()
+        {
+            Action testCode =
+                () => _converter.ConvertBack(Visibility.Collapsed, typeof (Visibility), null, string.Empty);
+
+            Assert.Throws<NotImplementedException>(testCode);
+        }
+    }
+}
