1c734a96dbbea43a4b7caa60ba03800185f2f273
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2015-12-08 23:21:11 -0200

    Added new tested converter: DateTimeToTimeSpanConverter.

---
+++ MagicBox.UWP.Tests/Converters/DateTimeToTimeSpanConverterFixture.cs
@@ -0,0 +1,51 @@
+﻿using MagicBox.UWP.Converters;
+using MagicBox.UWP.Tests.Interfaces;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System;
+using Windows.UI.Xaml.Data;
+
+namespace MagicBox.UWP.Tests.Converters
+{
+    /// <summary>
+    /// A test class for <seealso cref="DateTimeToTimeSpanConverter"/>.
+    /// </summary>
+    [TestClass]
+    public sealed class DateTimeToTimeSpanConverterFixture : IConverterTestable, ITestable
+    {
+        private IValueConverter _converter;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _converter = new DateTimeToTimeSpanConverter();
+        }
+
+        [TestMethod]
+        public void VerifyConverter()
+        {
+            var mockedValue = new DateTime(2014, 09, 18, 14, 22, 20);
+            var actual = (TimeSpan)_converter.Convert(mockedValue, typeof(TimeSpan), null, string.Empty);
+            var expected = new TimeSpan(14, 22, 20);
+
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void VerifyConverterBack()
+        {
+            var mockedValue = new TimeSpan(14, 22, 20);
+            var actual = (DateTime)_converter.ConvertBack(mockedValue, typeof(TimeSpan), null, string.Empty);
+            var expected = DateTime.Today.Add(new TimeSpan(14, 22, 20));
+
+            Assert.IsNotNull(actual); ;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void VerifyInitialization()
+        {
+            Assert.IsNotNull(_converter);
+        }
+    }
+}
---
+++ MagicBox.UWP/Converters/DateTimeToTimeSpanConverter.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace MagicBox.UWP.Converters
+{
+    /// <summary>
+    /// Converter a DateTime value to its correspondent in TimeSpan value.
+    /// </summary>
+    public sealed class DateTimeToTimeSpanConverter : IValueConverter
+    {
+        /// <summary>
+        /// Convert a DateTime value to TimeSpan.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType">The type indicator of TimeSpan.</param>
+        /// <param name="parameter">A parameter that can be used to change the normal flow of logic in the method.</param>
+        /// <param name="language">The language to be used on converter scope.</param>
+        /// <returns>A TimeSpan object.</returns>
+        /// <remarks>In case of the value is null, the DateTime to consider is the right now time.</remarks>
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            DateTime dateTime;
+
+            try
+            {
+                dateTime = (DateTime)value;
+            }
+            catch (Exception)
+            {
+                dateTime = DateTime.Now;
+            }
+
+            return dateTime.TimeOfDay;
+        }
+
+        /// <summary>
+        /// Convert back from a TimeSpan value to DateTime object.
+        /// </summary>
+        /// <param name="value">A TimeSpan value.</param>
+        /// <param name="targetType">The type indicator of TimeSpan.</param>
+        /// <param name="parameter">A parameter that can be used to change the normal flow of logic in the method.</param>
+        /// <param name="language">The language to be used on converter scope.</param>
+        /// <returns>A DateTime object.</returns>
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            TimeSpan timeSpan;
+
+            try
+            {
+                timeSpan = (TimeSpan)value;
+            }
+            catch (Exception)
+            {
+                timeSpan = new TimeSpan(DateTime.Now.Ticks);
+            }
+
+            var dateTime = DateTime.Today;
+
+            return dateTime.Add(timeSpan);
+        }
+    }
+}
